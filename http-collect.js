8052f1c3c218459dbc3f77e0fa99fe71a73da9e1
GABRIEL PAREDES <gabriel_paredes@bhdleon.com.do>
2022-05-13 22:04:27 -0400

    Add bl module and http-collect in two ways

--- http-collect.js
+++ http-collect.js
@@ -1,15 +1,22 @@
 const http =  require('http');
+const bl = require('bl');
 const url = process.argv[2].startsWith('http') ? process.argv[2] : 'http:\\'+ process.argv[2];
 
 http.get(url,(response)=>{
     response.setEncoding('utf8');
+    response.pipe(bl((err, data) => {
+        if(err) return console.error(err);
 
-    let dataCollected = '';
-    response.on('data', data => { 
-        dataCollected += data;
-    } )
-    response.on('end',() => {
-        console.log(dataCollected.length);
-        console.log(dataCollected);
-    })
+        console.log(data.length);
+        console.log(data.toString());
+    }))
+
+    // let dataCollected = '';
+    // response.on('data', data => { 
+    //     dataCollected += data;
+    // } );
+    // response.on('end',() => {
+    //     console.log(dataCollected.length);
+    //     console.log(dataCollected);
+    // });
 }).on('error', console.error)
